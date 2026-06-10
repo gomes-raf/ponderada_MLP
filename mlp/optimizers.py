2f7869471a150d683e29f09e575c217b79cb175e
Rafael Gomes <rafael.gomes@sou.inteli.edu.br>
2026-06-09 22:48:16 -0300

    feat: implementa otimizador

---
+++ mlp/optimizers.py
@@ -0,0 +1,9 @@
+# SGD e opcionais
+
+class SGD:
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+
+    def update(self, params, grads):
+        for key in params:
+            params[key] -= self.learning_rate * grads[key]
